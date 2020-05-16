c8edaea46a37278e5b199e0c440c1be94c101c25
Ranjeet Mallipeddi <43417434+flash29@users.noreply.github.com>
2020-05-16 08:44:29 +0530

    adding font.tff

--- pong-1/main.lua
+++ pong-1/main.lua
@@ -8,8 +8,15 @@ VIRTUAL_HEIGHT = 243
 
 
 function love.load()
-
     love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    -- more "retro-looking" font object we can use for any text
+    smallFont = love.graphics.newFont('font.ttf', 8)
+
+    -- set LÖVE2D's active font to the smallFont obect
+    love.graphics.setFont(smallFont)
+
+    -- initialize window with virtual resolution
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         resizable = false,
@@ -27,8 +34,30 @@ end
 
 
 function love.draw()
- 
+    -- begin rendering at virtual resolution
     push:apply('start')
-    love.graphics.printf('Hello Pong!', 0, VIRTUAL_HEIGHT / 2 - 6, VIRTUAL_WIDTH, 'center')
+
+    -- clear the screen with a specific color; in this case, a color similar
+    -- to some versions of the original Pong
+    love.graphics.clear(40, 45, 52, 255)
+
+    -- draw welcome text toward the top of the screen
+    love.graphics.printf('Hello Pong!', 0, 20, VIRTUAL_WIDTH, 'center')
+
+    --
+    -- paddles are simply rectangles we draw on the screen at certain points,
+    -- as is the ball
+    --
+
+    -- render first paddle (left side)
+    love.graphics.rectangle('fill', 10, 30, 5, 20)
+
+    -- render second paddle (right side)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50, 5, 20)
+
+    -- render ball (center)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
+
+    -- end rendering at virtual resolution
     push:apply('end')
 end
